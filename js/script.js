49e6234fdc62395512949893117b4c80d31af234
||We(r)K3|R|| <122714529+Pixelgameweb@users.noreply.github.com>
2024-01-19 22:19:31 +0200

    add files

---
+++ js/script.js
@@ -0,0 +1,50 @@
+// let Car = {      //1, 2, 5, 12 и 7 задания  
+// color: "orange", 
+// engine: "2L",
+// brand: "toyota",
+//     driver: {
+//         name: "Иван",
+//         old: 19,
+//         category: "Категория б",
+//     },
+//     driver2: {
+//         name: "Данила",
+//         category: "Категория А и б",
+//     }
+// };
+// let driver = {//3 задание
+//     name: "danila",
+//     lastname: "bodrov",
+//     old: 19,
+//     category: "a и b",
+// }
+// console.log(Car); 4 задание
+
+// let driver = {name: "Иван",category: "Категория б", old: 19}  6 задание
+// for( let name in driver){
+//     console.log("driver." + name + "=" + driver[name]);
+// }
+// for( let category in driver){
+//     console.log("driver." + category + "=" + driver[category]);
+// }
+// for( let old in driver){
+//     console.log("driver." + old + "=" + driver[old]);
+// }
+// if(personalbar.hasOwnProperty("model")){  9 задание 
+//     console.log(Car);
+// }
+// else console.log("значения нет");
+
+// let driver = {name: "Иван",category: "Категория б", old: 19}8 и 10 задание
+// console.log(driver)
+
+// console.log(typeof "orange"); 11 задание
+// console.log(typeof "toyota");
+// console.log(typeof "2L");
+
+// console.log(Car); 13 задание
+
+// let driver2 = "driver2"; 14 и 15 задания
+// console.log(driver2)
+
+
